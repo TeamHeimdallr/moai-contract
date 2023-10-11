a9b5ee2d892289e7a123296752ee597af028e9fa
lucky7323 <eunho.ambition@gmail.com>
2023-10-11 12:19:08 +0900

    feat: weighted pool factory deployment scripts

---
+++ contracts/scripts/4_weighted-pool-factory.ts
@@ -0,0 +1,91 @@
+import { ethers } from "hardhat";
+
+import fs from "fs";
+import path from "path";
+import { networkMap } from "../networks";
+
+async function main() {
+  const DEPLOYMENT_DIRECTORY = path.resolve(__dirname, "../deployed");
+  const DEPLOYMENT_HISTORY_DIRECTORY = path.resolve(
+    __dirname,
+    "../deployed-histories"
+  );
+
+  console.log("deploying WeightedPoolFactory");
+  try {
+    const provider = ethers.provider;
+    const network = (await provider.getNetwork()).chainId;
+    const networkName = networkMap[network];
+
+    // deployed
+    const filePath = path.join(DEPLOYMENT_DIRECTORY, `${networkName}.json`);
+    const fileExists =
+      fs.existsSync(filePath) && fs.statSync(filePath).isFile();
+
+    const newFileContents: Record<string, string> = fileExists
+      ? JSON.parse(fs.readFileSync(filePath).toString())
+      : {};
+
+    // deployed histories
+    const historiesFilePath = path.join(
+      DEPLOYMENT_HISTORY_DIRECTORY,
+      `${networkName}.json`
+    );
+    const historiesFileExists =
+      fs.existsSync(historiesFilePath) &&
+      fs.statSync(historiesFilePath).isFile();
+
+    const historiesFileContents: Record<string, string> = historiesFileExists
+      ? JSON.parse(fs.readFileSync(historiesFilePath).toString())
+      : {};
+
+    const from = await provider.getSigner().getAddress();
+
+    console.log("from:", from, "network:", networkName);
+
+    const factory = await ethers.getContractFactory("WeightedPoolFactory");
+
+    const vault = newFileContents["Vault"];
+    const protocolFeePercentagesProvider =
+      newFileContents["ProtocolFeePercentagesProvider"];
+
+    const BaseVersion = { version: 4, deployment: "20230320-weighted-pool-v4" };
+    const factoryVersion = JSON.stringify({
+      name: "WeightedPoolFactory",
+      ...BaseVersion,
+    });
+    const poolVersion = JSON.stringify({
+      name: "WeightedPool",
+      ...BaseVersion,
+    });
+
+    const ret = await factory.deploy(
+      vault,
+      protocolFeePercentagesProvider,
+      factoryVersion,
+      poolVersion
+    );
+    const now = new Date();
+    const timestamp = Math.floor(now.getTime() / 1000);
+
+    newFileContents["WeightedPoolFactory"] = ret.address;
+    historiesFileContents[`WeightedPoolFactory-${timestamp}`] = ret.address;
+
+    console.log("contract addr:", ret.address);
+
+    fs.writeFileSync(filePath, JSON.stringify(newFileContents, null, 2));
+    fs.writeFileSync(
+      historiesFilePath,
+      JSON.stringify(historiesFileContents, null, 2)
+    );
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
