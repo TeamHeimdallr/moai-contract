2d6fb072f16deaccfac41de902e73c0dcd89a282
lucky7323 <eunho.ambition@gmail.com>
2024-03-20 18:23:40 +0900

    feat: test erc20 token contract

---
+++ contracts/dex/TestToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract TestToken is ERC20, ERC20Permit {
+    constructor() ERC20("TestToken", "TestToken") ERC20Permit("TestToken") {}
+}
---
+++ scripts/10_test_token.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ "$1" != "--rpc" ]; then
+    echo "Invalid option. Use --rpc"
+    exit 1
+fi
+
+forge create \
+  --rpc-url "$2" \
+  --private-key $CALLER_PRIVATE_KEY \
+  --optimize \
+  --optimizer-runs 9999 \
+  --legacy \
+  --verify \
+  --verifier sourcify contracts/dex/TestToken.sol/:TestToken
