ecdc7a553ecb7db4824fd8bc9ee4a6963cc45174
lucky7323 <eunho.ambition@gmail.com>
2024-04-01 16:47:15 +0900

    feat: stable pool info aggregator

--- scripts/9_composable_stable_pool_factory.sh
+++ scripts/9_composable_stable_pool_factory.sh
@@ -13,7 +13,7 @@ forge create \
   --legacy \
   --verify \
   --verifier sourcify contracts/dex/composable-stable-pool-v5/ComposableStablePoolFactory.sol/:ComposableStablePoolFactory \
-  --constructor-args 0xc922770de79fc31Cce42DF3fa8234c864fA3FeaE 0xCf39Dd95da35064c0C62b2e5dcCC15e7ACBc4CEb {"name":"ComposableStablePoolFactory","version":5,"deployment":"20230711-composable-stable-pool-v5"} {"name":"ComposableStablePool","version":5,"deployment":"20230711-composable-stable-pool-v5"}
+  --constructor-args 0x1D6B655289328a1083EcD70170692002dBED1aBD 0x2Cfc4e04a825286e2B54b1281De1D2AD43EB254F {"name":"ComposableStablePoolFactory","version":5,"deployment":"20230711-composable-stable-pool-v5"} {"name":"ComposableStablePool","version":5,"deployment":"20230711-composable-stable-pool-v5"}
 
 # {
 # IVault vault,
